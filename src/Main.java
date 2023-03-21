32beae7c8b207a4ddf95bcf891137e6f2f43165c
Alex Bondarenko <albom2004q@gmail.com>
2023-03-21 19:08:43 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Mishanya, this project is our future");
+        Scanner scanner = new Scanner(System.in);
+        byte age = scanner.nextByte();
+        System.out.println("Your age is: " + age);
+    }
+    
+}
